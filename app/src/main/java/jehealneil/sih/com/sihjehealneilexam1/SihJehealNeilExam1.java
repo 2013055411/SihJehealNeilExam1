350c867def59a98f9ffd0f378158f88bc99ac4e4
Jeheal Neil Sih <2013055411@ust-ics.mygbiz.com>
2018-10-06 16:13:22 +0800

    Initial commit

---
+++ app/src/main/java/jehealneil/sih/com/sihjehealneilexam1/SihJehealNeilExam1.java
@@ -0,0 +1,13 @@
+package jehealneil.sih.com.sihjehealneilexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SihJehealNeilExam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sih_jeheal_neil_exam1);
+    }
+}
